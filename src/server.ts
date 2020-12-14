c0443784a15f2f2efe20aa6b148ca9cc395733cf
pedro.santos <pedroabs.dev42@gmail.com>
2020-12-14 16:13:28 -0300

    minor updates

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log('API running at port 3333...'));
+app.get('/healthcheck', (request, response) => {
+  return response.status(200).json({
+    uptime: process.uptime(),
+  });
+});
+
+app.listen(3333, () => {
+  console.log('API running at port 3333...');
+});
